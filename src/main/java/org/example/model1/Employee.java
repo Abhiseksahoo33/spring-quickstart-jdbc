08c822c85544270c9cb1788ebc04c826b7d9f721
abhisek <abhiseksahoo33@gmail.com>
2020-12-16 19:41:09 +0530

    initial commit | Dependancy added

---
+++ src/main/java/org/example/model1/Employee.java
@@ -0,0 +1,47 @@
+package org.example.model1;
+
+public class Employee {
+    private  Integer id;
+    private  Integer uid;
+    private  String firstname;
+    private  String lastname;
+
+    public Employee(Integer id, Integer uid, String firstname, String lastname) {
+        this.id = id;
+        this.uid = uid;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
